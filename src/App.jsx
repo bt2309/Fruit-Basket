9221fbdc67a7d9ed3dc8b8be165e6f9bc05762a4
bt23 <brandonyxteo@gmail.com>
2024-08-12 23:17:54 +0800

    readme-update

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <h1>Fruits Basket</h1>
+            <nav>
+                <h2>Fruits List</h2>
+                <h2>Cart</h2>
+            </nav>
+        </>
+    )
+}
+
+export default App;
---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+// import debug from "debug";
+
+// const log = debug("fruits:services:api");
+
+// export async function getData() {
+//     const url = "";
+//     try {
+//         const response = await fetch(url, {
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 Authorization:
+//                 "Bearer"
+//             }
+//         })
+//     }
+// }
